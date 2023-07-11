3caf41128acc9e4db639dd52df428359c9eec0d3
Neykem <neykem.css@gmail.com>
2023-07-11 13:39:33 +0300

    Add basic type

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
