1ebb92b015fb6b7a507c6d3dcd0b242409cc9ac1
Neykem <neykem.css@gmail.com>
2023-07-11 14:20:10 +0300

    _

--- Endpoints/CatalogEndpoints/GetAll.cs
+++ Endpoints/CatalogEndpoints/GetAll.cs
@@ -1,13 +1,14 @@
 ﻿using Ardalis.ApiEndpoints;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using WebEndpoint.Endpoints.CatalogEndpoints.CatalogResult;
 
 namespace WebEndpoint.Endpoints.CatalogEndpoints
 {
     public class GetAll : EndpointBaseAsync.WithRequest<int>
-      .WithActionResult<AuthorResult>
+      .WithActionResult<BookResult>
     {
-        public override Task<ActionResult<AuthorResult>> HandleAsync(int request, CancellationToken cancellationToken = default)
+        public override Task<ActionResult<BookResult>> HandleAsync(int request, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
