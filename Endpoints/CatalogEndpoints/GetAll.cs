0b8e0c2cad6c2e0fd99b84712d1c149eb5a56d7f
Neykem <neykem.css@gmail.com>
2023-07-11 14:48:22 +0300

    Delete def basic api

--- Endpoints/CatalogEndpoints/GetAll.cs
+++ Endpoints/CatalogEndpoints/GetAll.cs
@@ -8,7 +8,19 @@ namespace WebEndpoint.Endpoints.CatalogEndpoints
     public class GetAll : EndpointBaseAsync.WithRequest<int>
       .WithActionResult<BookResult>
     {
-        public override Task<ActionResult<BookResult>> HandleAsync(int request, CancellationToken cancellationToken = default)
+        private readonly Logger<GetAll> _logger;
+        private readonly IMapper _mapper;
+        public GetAll(Logger<GetAll> logger, IMapper mapper)
+        {
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Get all books 
+        /// </summary>
+        [HttpGet("api/[namespace]/{id}", Name = "[namespace]_[controller]")]
+        public override async Task<ActionResult<BookResult>> HandleAsync(int request, CancellationToken cancellationToken = default)
         {
             throw new NotImplementedException();
         }
